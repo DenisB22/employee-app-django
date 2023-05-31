59d4d2271211da3db565abf2b5e294ab89a2b634
DenisB22 <bechiragich97@gmail.com>
2023-05-31 14:13:57 +0300

    Changed Database Settings

--- employees/settings.py
+++ employees/settings.py
@@ -96,15 +96,19 @@ WSGI_APPLICATION = 'employees.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': "django.db.backends.mysql",
+#         'NAME': "employees",
+#         'USER': "root",
+#         'PASSWORD': "PythonDev1997",
+#         'HOST': "127.0.0.1",
+#         'PORT': "3306",
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.mysql",
-        'NAME': "employees",
-        'USER': "root",
-        'PASSWORD': "PythonDev1997",
-        'HOST': "127.0.0.1",
-        'PORT': "3306",
-    }
+    'default': dj_database_url.config(),
 }
 
 
